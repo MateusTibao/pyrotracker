1e610c41cdb65879001e469ef7c045f3a76e9654
MateusTibao <mateus.tibao.3@gmail.com>
2025-05-31 20:07:42 -0300

    feat: primeiro commit

---
+++ src/main/java/br/com/pyrotracker/PyrotrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.pyrotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyrotrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyrotrackerApplication.class, args);
+	}
+
+}
